71a132df58a16eaf4239a44448e168b58889cb78
sergo <sergmy4@yandex.ru>
2021-12-05 22:57:31 +0300

    Добавил пинг гугла и убрал секреты

--- src/main/java/org/budgetmanager/BudgetManagerApplication.java
+++ src/main/java/org/budgetmanager/BudgetManagerApplication.java
@@ -2,8 +2,10 @@ package org.budgetmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BudgetManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BudgetManagerApplication.class, args);
